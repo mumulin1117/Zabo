c64db490f7074bee3c5a21bd09df37b249fa70ab
mumulin1117 <mumulin1117@163.com>
2025-10-10 14:23:43 +0800

    Update AppDelegate.swift

--- ZioabosFuli/AppDelegate.swift
+++ ZioabosFuli/AppDelegate.swift
@@ -99,7 +99,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         reverb = VocalType.crystalline
         voiceSphere()
         var Ayeuyi:Float = 34
